d8a6c99e4f015ebf6523e0e8e380c4ae10c27e7f
Lucas <zubiarrainlucas@gmail.com>
2025-01-14 15:09:06 +0100

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="flex h-screen justify-center items-center">
+      <Button className=''> Hello world</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
