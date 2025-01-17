dacdcc722e2a0b657baf448558e3757b5667c261
Lucas <zubiarrainlucas@gmail.com>
2025-01-17 10:45:42 +0100

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,32 @@
-import LoginPage from "./login/page"
-// LoginPage
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './login/page';
+
 function App() {
-  
-
   return (
-    
-    <div className="flex h-screen justify-center items-center">
-      <LoginPage />
-    </div>
-
-  )
+    <Router>
+      <div className="flex h-screen justify-center items-center">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="">
+                <LoginPage />
+              </div>
+            }
+          />
+          <Route
+            path="/test"
+            element={
+              <div className="">
+                test
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
